da1561aa7204a226437b11050163277141d574cc
luismiguel172 <luis.goncalves68@etec.sp.gov.br>
2023-05-18 22:13:56 -0300

    Criação e atualização de páginas

--- app/src/main/java/br/com/etecia/myapp/Login_Activity.java
+++ app/src/main/java/br/com/etecia/myapp/Login_Activity.java
@@ -2,13 +2,62 @@ package br.com.etecia.myapp;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.google.android.material.textfield.TextInputEditText;
+
+import br.com.etecia.myapp.Menu_Activity;
 
 public class Login_Activity extends AppCompatActivity {
+    Button btnEntrar, btnSair;
+    TextInputEditText txtUsuario, txtSenha;
+    TextView lblCadastraUsuario;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.login_layout);
+
+        btnEntrar = findViewById(R.id.btnEntrar);
+        btnSair = findViewById(R.id.btnSair);
+        txtUsuario = findViewById(R.id.txtUsuario);
+        txtSenha = findViewById(R.id.txtSenha);
+        lblCadastraUsuario = findViewById(R.id.lblCadastrarUsuario);
+
+        btnSair.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                finish();
+            }
+        });
+        btnEntrar.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String usuario, senha;
+
+                usuario = txtUsuario.getText().toString();
+                senha = txtSenha.getText().toString();
+
+                if (usuario.equals("etecia") && senha.equals("etecia")) {
+                    startActivity(new Intent(getApplicationContext(), Menu_Activity.class));
+                } else {
+                    Toast.makeText(getApplicationContext(),
+                            "Usuário ou senha inválidos",
+                            Toast.LENGTH_SHORT).show();
+                }
+            }
+        });
+        lblCadastraUsuario.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(getApplicationContext(),
+                        Cadastro_Activity.class));
+            }
+        });
     }
 }
--- app/src/main/java/br/com/etecia/myapp/Menu_Activity.java
+++ app/src/main/java/br/com/etecia/myapp/Menu_Activity.java
@@ -2,13 +2,63 @@ package br.com.etecia.myapp;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
-public class Menu_Activity extends AppCompatActivity {
+import com.google.android.material.card.MaterialCardView;
+
+public class Menu_Activity extends AppCompatActivity implements View.OnClickListener{
+    MaterialCardView idMenuPets, idMenuProdutos, idMenuDoacoes, idMenuServicos, idMenuLocalizacao, idMenuParceiros;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.menu_layout);
+
+        idMenuPets = findViewById(R.id.idMenuPets);
+        idMenuProdutos = findViewById(R.id.idMenuProdutos);
+        idMenuDoacoes = findViewById(R.id.idMenuDoacoes);
+        idMenuServicos = findViewById(R.id.idMenuServicos);
+        idMenuLocalizacao = findViewById(R.id.idMenuLocalizacao);
+        idMenuParceiros = findViewById(R.id.idMenuParceiros);
+
+        idMenuPets.setOnClickListener(this);
+        idMenuProdutos.setOnClickListener(this);
+        idMenuDoacoes.setOnClickListener(this);
+        idMenuServicos.setOnClickListener(this);
+        idMenuLocalizacao.setOnClickListener(this);
+        idMenuParceiros.setOnClickListener(this);
+
+    }
+
+    @Override
+    public void onClick(View v) {
+        switch (v.getId()){
+            case R.id.idMenuPets:
+                startActivity(new Intent(getApplicationContext(),
+                    Pets_Activity.class));
+                break;
+            case R.id.idMenuProdutos:
+                startActivity(new Intent(getApplicationContext(),
+                        Alimentos_Activity.class));
+                break;
+            case R.id.idMenuDoacoes:
+                startActivity(new Intent(getApplicationContext(),
+                        Doacoes_Activity.class));
+                break;
+            case R.id.idMenuServicos:
+                startActivity(new Intent(getApplicationContext(),
+                        Servicos_Activity.class));
+                break;
+            case R.id.idMenuLocalizacao:
+                startActivity(new Intent(getApplicationContext(),
+                        Localizacao_Activity.class));
+                break;
+            case R.id.idMenuParceiros:
+                startActivity(new Intent(getApplicationContext(),
+                        Parceiros_Activity.class));
+                break;
+        }
     }
 }
